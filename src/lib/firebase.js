1022b349f7288ddb89b648d744efb0980f874516
imk0315 <victory03070@gmail.com>
2026-02-20 23:47:56 +0800

    修正firebase設定

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,18 +1,16 @@
-
-
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPNzlN9Lq1wXSXvpvKeQhBN1QZs1nDu8E",
   authDomain: "love-quiz-fd802.firebaseapp.com",
- databaseURL: "https://love-quiz-fd802-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://love-quiz-fd802-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "love-quiz-fd802",
-  storageBucket: "love-quiz-fd802.firebasestorage.app",
-  messagingSenderId: ""163599587575",
+  storageBucket: "love-quiz-fd802.appspot.com",
+  messagingSenderId: "163599587575",
   appId: "1:163599587575:web:dd9e7b78b9639690299bf2",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-
